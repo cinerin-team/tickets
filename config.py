5da945cd0f77650be08b15b455f3f18d4d8697d2
Miklos Parrag <miklos.parrag@ericsson.com>
2024-07-29 08:45:03 +0200

    w tag

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # these variables should be updated!!!
-SPRINT_ID = 21398
+SPRINT_ID = 21678
 TREX_QUERY_FOR_ALL_CINERIN_TCS_RELEASE = 'https://epgweb.sero.wh.rnd.internal.ericsson.com/treqsviewer/viewer?{%22product_part%22:%22build%22,%22product%22:%22EPG_3.38_28%22,%22build%22:%22latest%22,%22type%22:%22testcase%22,%22selected%22:[%22testcase_id%22,%22build_time%22,%22job_time%22],%22filters%22:{%22parent_revision%22:[%22true%22]}}'
-SPRINT_START_DATE = "2024-06-17"
+SPRINT_START_DATE = "2024-07-29"
 SPRINT_WEEKS = 3
 MAIN = "EPG3.39"
 RELEASE = "EPG3.38"

4ae6530fa7a0f9daa3ae9f25592a69ed7868f37b
Miklos Parrag <miklos.parrag@ericsson.com>
2024-08-03 09:44:05 +0200

    3.40 master

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ SPRINT_ID = 21678
 TREX_QUERY_FOR_ALL_CINERIN_TCS_RELEASE = 'https://epgweb.sero.wh.rnd.internal.ericsson.com/treqsviewer/viewer?{%22product_part%22:%22build%22,%22product%22:%22EPG_3.38_28%22,%22build%22:%22latest%22,%22type%22:%22testcase%22,%22selected%22:[%22testcase_id%22,%22build_time%22,%22job_time%22],%22filters%22:{%22parent_revision%22:[%22true%22]}}'
 SPRINT_START_DATE = "2024-07-29"
 SPRINT_WEEKS = 3
-MAIN = "EPG3.39"
-RELEASE = "EPG3.38"
-RELEASE_NEEDED = False
+MAIN = "EPG3.40"
+RELEASE = "EPG3.39"
+RELEASE_NEEDED = True
 
 # these variables should not be updated
 TREX_QUERY_FOR_ALL_CINERIN_TCS_MAIN = 'https://epgweb.sero.wh.rnd.internal.ericsson.com/treqsviewer/viewer?{%22product_part%22:%22build%22,%22product%22:%22EPG_MASTER_28%22,%22build%22:%22latest%22,%22type%22:%22testcase%22,%22selected%22:[%22testcase_id%22,%22build_time%22,%22job_time%22],%22filters%22:{%22parent_revision%22:[%22true%22],%22Responsible_Team%22:%22Cinerin%22}}'

6fe24e5e55cd734c791bccf2e6f5f1eb6f000b6f
etotist <4@4.com>
2024-11-29 16:31:58 +0100

    sprint gen 11-29

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # these variables should be updated!!!
-SPRINT_ID = 22444
+SPRINT_ID = 22588
 TREX_QUERY_FOR_ALL_CINERIN_TCS_RELEASE = 'https://epgweb.sero.wh.rnd.internal.ericsson.com/treqsviewer/viewer?{%22product_part%22:%22build%22,%22product%22:%22EPG_3.38_28%22,%22build%22:%22latest%22,%22type%22:%22testcase%22,%22selected%22:[%22testcase_id%22,%22build_time%22,%22job_time%22],%22filters%22:{%22parent_revision%22:[%22true%22]}}'
-SPRINT_START_DATE = "2024-11-11"
+SPRINT_START_DATE = "2024-12-02"
 SPRINT_WEEKS = 3
 MAIN = "EPG3.41"
 RELEASE = "EPG3.40"
